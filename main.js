0e04ab913883b95abd104b075420bbb75f3477d2
SNT23SK <snt23sk@gmail.com>
2021-10-24 17:41:30 +0400

    added case start

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ const player1 = {
 };
 const player2 = {
 	player: 2,
-	name: 'Kitana',
+	name: 'Sub Zero',
 	hp: 100,
-	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-	weapon: 'knife',
+	img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+	weapon: 'ice',
 	attack: function fight() {
 		console.log(this.name + ' fight... ');
 	},
@@ -56,10 +56,10 @@ const player4 = {
 };
 const player5 = {
 	player: 5,
-	name: 'Sub Zero',
+	name: 'Kitana',
 	hp: 100,
-	img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-	weapon: 'ice',
+	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+	weapon: 'knife',
 	attack: function fight() {
 		console.log(this.name + ' fight... ');
 	},
@@ -74,6 +74,69 @@ const HIT = {
 	foot: 20,
 };
 const ATTACK = ['head', 'body', 'foot'];
+const logs = {
+	start: 'Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.',
+	end: [
+		'Результат удара [playerWins]: [playerLose] - труп',
+		'[playerLose] погиб от удара бойца [playerWins]',
+		'Результат боя: [playerLose] - жертва, [playerWins] - убийца',
+	],
+	hit: [
+		'[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.',
+		'[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.',
+		'[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.',
+		'[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.',
+		'[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.',
+		'[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.',
+		'[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.',
+		'[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.',
+		'[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.',
+		'[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.',
+		'[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.',
+		'[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.',
+		'[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.',
+		'[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.',
+		'[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.',
+		'[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.',
+		'[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.',
+		'[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.',
+	],
+	defence: [
+		'[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.',
+		'[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.',
+		'[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.',
+		'[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.',
+		'[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.',
+		'[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.',
+		'[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.',
+		'[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.',
+	],
+	draw: 'Ничья - это тоже победа!',
+};
+function generateLog(type, player1, player2) {
+	switch (type) {
+		case 'start':
+			const today = new Date();
+			const time = `${today.getHours()}:${today.getMinutes()}`;
+			const start = logs[type]
+				.replace('[time]', time)
+				.replace('[player1]', player1.name)
+				.replace('[player2]', player2.name);
+			$chat.insertAdjacentHTML('afterbegin', start);
+			break;
+		case 'end':
+			console.log('#### end: ');
+			break;
+		case 'hit':
+			const random = getRandom(logs[type].length - 1);
+			const text = logs[type][random]
+				.replace('[playerKick]', player1.name)
+				.replace('[playerDefence]', player2.name);
+			const el = `<p>${text}</p>`;
+			$chat.insertAdjacentHTML('afterbegin', el);
+			break;
+	}
+}
 
 function createElement(tag, className) {
 	const $tag = document.createElement(tag);
@@ -189,6 +252,7 @@ function checkAttack(enemy, hero) {
 	if (hero.hit !== enemy.defence) {
 		hit = hero.value;
 	}
+
 	return hit;
 }
 
@@ -202,9 +266,12 @@ $formFight.addEventListener('submit', function (e) {
 	const enemy = enemyAttack();
 	const hero = heroAttack();
 	getDamage(player1, checkAttack(enemy, hero));
+	generateLog('hit', player2, player1);
 	getDamage(player2, checkAttack(hero, enemy));
+	generateLog('hit', player1, player2);
 	checkWin();
 });
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
+generateLog('start', player1, player2);
