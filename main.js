6552d204af9bbf787eb9c36fcbd8af6bef6d4068
SNT23SK <snt23sk@gmail.com>
2021-10-28 20:01:29 +0400

    preparing for class creation Game

--- main.js
+++ main.js
@@ -1,15 +1,2 @@
-import { generateLog } from './logs.js';
-import { createPlayer, player1, player2 } from './player.js';
-import { checkAttack, attack, enemyAttack, checkWin } from './game.js';
-import { $arenas, $formFight } from './game.js';
-
-$formFight.addEventListener('submit', function (e) {
-	e.preventDefault();
-	const enemy = enemyAttack();
-	const hero = attack();
-	checkAttack(enemy, hero);
-	checkWin();
-});
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
-generateLog('start', player1, player2);
+import { start } from './game.js';
+start();
